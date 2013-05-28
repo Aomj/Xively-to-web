13bb37cfe1b36fd9168b76e8bf84d379b85425d8
Anselmo <anselmo.meira@hotmail.com>
2013-05-27 23:12:09 -0300

    Fully functional, current and history feed

--- StatusXively.php
+++ StatusXively.php
@@ -1,6 +1,6 @@
 <?php
 
-include('PachubeAPI.php');
+include('XivelyAPI.php');
 
 define( "COSMAPIKEY", "MJ5wFELHdgBtj35u9CQfpETwhgMZ1OCniZATwSqLSHUZnOCf" );
 define( "FEEDID", 117535 );
@@ -9,15 +9,18 @@ define( "FEEDID", 117535 );
 $cosmFeed = new PachubeAPI(COSMAPIKEY);
 
 
+//change $ver to swap between current version and history
+$ver = 'Hist';
+
+		switch($ver){ 
+		
+		//get the current value 
+		case 'current':
+
 			// Get the feed
 			$jsonResult =  $cosmFeed->getDatastream('json', FEEDID, '0102');
 			
-			var_dump($jsonResult);			
-			//echo($jsonResult);
-			
-			// Get the current value and format output result
-			//$currentValue = $jsonResult->current_value;
-			
+			// Decoding the feed to get the result
 			$jsonDecoded = json_decode($jsonResult);
 			
 
@@ -26,45 +29,38 @@ $cosmFeed = new PachubeAPI(COSMAPIKEY);
 			header( 'Cache-Control: no-cache' );
 			
 			echo "current value: $jsonDecoded->current_value";
-		
-
-
-
+			break;
 
 
 	
-		// This is a grab log call
-	//case 'log':
+		// History call
+		case 'Hist':
 			// Retrieve the log for the past 24 hours with
 			// an interval of 30 minutes between each
-			// datapoint
-			//$startTime 		= date( 'Y-m-d\TG:i:s', strtotime('-24 hours') );
-			//$endTime 		= date( 'Y-m-d\TG:i:s' );
+			// datapoint, change the interval to get different range 
+			$startTime 		= date( 'Y-m-d\TG:i:s', strtotime('-24 hours') );
+			$endTime 		= date( 'Y-m-d\TG:i:s' );
 
 			// Get the feed history
-		//	$jsonResult = json_decode( $cosmFeed->getFeedHistory('json', FEEDID, $startTime, $endTime, $duration=false, $page=false, $per_page=false, $time=false, $find_previous=false, $interval_type='discrete', $interval=3600) );
-
+			$jsonResult = $cosmFeed->getFeedHistory('json', FEEDID, $startTime, $endTime, $duration=false, $page=false, $per_page=false, $time=false, 				$find_previous=false, $interval_type='discrete', $interval=3600);
+			
 			// Get the datapoints array from the result set only
-	//		$datapoints = $jsonResult->datastreams[0]->datapoints;
+			$jsonDecoded = json_decode($jsonResult);
+			$datapoints = $jsonDecoded->datastreams[0]->datapoints;
+			
 
-			// BUG Here, that's why we need to revert back to UTC
-			// needs to be fixed..
-	//		date_default_timezone_set('Asia/Beirut');
-
-			// Fix the time formatting
-	//		foreach( $datapoints as &$dp )
-	//		{	
-//				$dp->at = date( "c", strtotime( $dp->at ) );
-		//	}
-
-		//	header( 'Content-Type: application/json' );
-		//	header( 'Access-Control-Allow-Origin: *' );
-		//	header( 'Cache-Control: no-cache' );
-		//	echo json_encode( array( "log" => $datapoints ) );
-		//	break;
-//	}
-
-//}
+			// Outpout the Result
+			foreach( $datapoints as $dp )
+			{
+				if(property_exists($dp, "value") && property_exists($dp, "at"))	{
+				$va = $dp->value;
+				$at = $dp->at;
+			
+				echo "value:   $va at $at <br/>";
+				}
+			}
+			break;
+		}
 
 
 ?>
